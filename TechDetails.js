95eca4960a931165f5a722ee8fa31a73a8d5f882
FredNBT <Fred.tucker@nbt.ag>
2018-10-21 10:35:51 +0200

    Tech-Details animation

--- TechDetails.js
+++ TechDetails.js
@@ -1,33 +1,54 @@
 
 let i = 0;
+let topcard = 0;
 var LineChartTimer = setInterval(function() {
 
   i +=1;
   let Sims =  ['img/simO2.png', 'img/simVodafone.png', 'img/simDeutcheTelekom.png', 'img/LycaTelekom.png']
-  let Countries =  ['UK', 'Steve Land', 'Germany', 'Poland']
-
+  let Countries =  ['UK', 'France', 'Germany', 'Ukraine']
+// reset counter
   if(i > Sims.length ){
     i = 0;
   }
 
-  
- 
-    console.log("flipped");
-    $('.card').toggleClass('flipped');
+  if (i === 0){
     $('#FrontImg').attr('src',Sims[i]);
     $('#CountryTextFront').html(Countries[i]);
+    $('#BackImg').attr('src',Sims[i+1]);
+    $('#CountryTextBack').html(Countries[i+1]);
+    topcard = 0;
+  }
+  else{
+  if (topcard === 0){
+    $('#BackImg').attr('src',Sims[i+1]);
+    $('#CountryTextBack').html(Countries[i+1]);
 
+    topcard = 1;
 
-if((i + 1) < Sims.length) {
-  $('#BackImg').attr('src',Sims[i+1]);
-  $('#CountryTextBack').html(Countries[i+1]);
-} else{
-  $('#BackImg').attr('src',Sims[0]);
-  $('#CountryTextBack').html(Countries[0]);
+ 
+    console.log("top");
+  }else{
+    $('#FrontImg').attr('src',Sims[i +1]);
+    $('#CountryTextFront').html(Countries[i +1]);
+    
+    topcard = 0;
+    
+ 
+    console.log("bottom");
+  }
+
 
 }
 
 
+
+  
+ 
+    console.log("flipped");
+   
+
+
+$('.card').toggleClass('flipped');
 
 
     
@@ -35,6 +56,3 @@ if((i + 1) < Sims.length) {
   },3500);
 
 
-  function flip() {
-   
-}
