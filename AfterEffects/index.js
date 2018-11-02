acc128068d682cba05c0b5928667e5b6ac2123d5
FredNBT <Fred.tucker@nbt.ag>
2018-11-02 13:07:58 +0100

    PCB WHite Added to Hardware Page

--- AfterEffects/index.js
+++ AfterEffects/index.js
@@ -1,7 +1,13 @@
-var animation = bodymovin.loadAnimation({
-    container: document.getElementById('bm'),
-    renderer: 'svg',
-    loop: true,
-    autoplay: true,
-    path: 'AfterEffects/data3.json'
-  })
+
+  document.addEventListener("DOMContentLoaded", function(event) {
+    var animation = bodymovin.loadAnimation({
+      container: document.getElementById('bm'),
+      renderer: 'svg',
+      loop: true,
+      autoplay: true,
+      path: 'AfterEffects/data3.json'
+    })
+  
+
+    console.log("AfterEffectsLoaded");
+  });
